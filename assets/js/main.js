e79432200109b84623f9ecb47b0df4482e118fb1
jramke <jstrmk@gmail.com>
2023-04-20 16:37:56 +0200

    add new font improve loading speed

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,27 +5,15 @@ import { Masonry } from "./masonry.js";
 (async function () {
     const data = await fetchData('./fonts.json');
     for(const font of data) {
-        await createPreviewItem(font.image, font.name, font.link);
+        createPreviewItem(font.image, font.name, font.link);
     }
     // masonry
     const parent = document.querySelector('#preview-list');
     const items = document.querySelectorAll('.preview-list__item');
-    // let resizeTimeout;
     const masonry = new Masonry(parent, items);
     await masonry.init();
     document.querySelector('#preloader').classList.add('ready');
 
-    // setTimeout(async () => {
-    //     await createPreviewItem(data[0].image, data[0].name, data[0].link);
-    // }, 5000);
-
-    // await setMasonryHeight(parent, items);
-    // window.onresize = function () {
-    //     clearTimeout(resizeTimeout);
-    //     resizeTimeout = setTimeout(function () {
-    //         setMasonryHeight(parent, items);
-    //     }, 100);
-    // };
 })();
 
 
@@ -43,39 +31,23 @@ async function fetchData(url) {
 }
 
 function createPreviewItem(image, name, link) {
-    return new Promise((resolve, reject) =>  {
-        const item = document.createElement('a');
-        item.href = link;
-        item.className = 'preview-list__item';
-        item.target = '_blank';
-        item.rel = "noopener noreferrer";
-        let imageElm = new Image(900);
-        // imageElm.setAttribute('data-src', image);
-        imageElm.src = image;
-        // imageElm.loading = 'lazy';
-        item.appendChild(imageElm);
-        item.insertAdjacentHTML(
-            'beforeend',
-            `<h2 class="visually-hidden">${name}</h2>`
-        );
-        const observer = new IntersectionObserver((entries, observer) =>  {
-            for(const entry of entries) {
-                if (entry.isIntersecting) {
-                    let image = entry.target;
-                    // image.classList.add('animate-in');
-                    // image.src = image.dataset.src;
-                    observer.unobserve(image);
-                }
-            }
-        })
-        observer.observe(imageElm);
-        // document.querySelector('#preview-list').appendChild(item);
+    const item = document.createElement('a');
+    item.href = link;
+    item.className = 'preview-list__item';
+    item.target = '_blank';
+    item.rel = "noopener noreferrer";
+    let imageElm = new Image(900);
+    imageElm.src = image;
+    item.appendChild(imageElm);
+    item.insertAdjacentHTML(
+        'beforeend',
+        `<h2 class="visually-hidden">${name}</h2>`
+    );
 
-        imageElm.onload = () => {
-            document.querySelector('#preview-list').appendChild(item);
-            resolve();
-        }
-    })
+    imageElm.onload = () => {
+        document.querySelector('#preview-list').appendChild(item);
+        // resolve();
+    }
 }
 
 
